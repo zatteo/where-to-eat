6692dcd000d15d1291d0cf6a7ca0a530928bb129
Théo Poizat <hello@zatteo.com>
2022-09-27 09:55:13 +0200

    Remove useless Restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show update destroy ]
+  # before_action :set_restaurant, only: %i[ show update destroy ]
 
   # GET /restaurants
   def index
@@ -9,43 +9,43 @@ class RestaurantsController < ApplicationController
   end
 
   # GET /restaurants/1
-  def show
-    render json: @restaurant
-  end
+  # def show
+  #   render json: @restaurant
+  # end
 
   # POST /restaurants
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
+  # def create
+  #   @restaurant = Restaurant.new(restaurant_params)
 
-    if @restaurant.save
-      render json: @restaurant, status: :created, location: @restaurant
-    else
-      render json: @restaurant.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @restaurant.save
+  #     render json: @restaurant, status: :created, location: @restaurant
+  #   else
+  #     render json: @restaurant.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /restaurants/1
-  def update
-    if @restaurant.update(restaurant_params)
-      render json: @restaurant
-    else
-      render json: @restaurant.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @restaurant.update(restaurant_params)
+  #     render json: @restaurant
+  #   else
+  #     render json: @restaurant.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /restaurants/1
-  def destroy
-    @restaurant.destroy
-  end
+  # def destroy
+  #   @restaurant.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
+    # def set_restaurant
+    #   @restaurant = Restaurant.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :type, :price, :city, :longitude, :latitude)
-    end
+    # def restaurant_params
+    #   params.require(:restaurant).permit(:name, :type, :price, :city, :longitude, :latitude)
+    # end
 end
--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -9,30 +9,4 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     get restaurants_url, as: :json
     assert_response :success
   end
-
-  test "should create restaurant" do
-    assert_difference("Restaurant.count") do
-      post restaurants_url, params: { restaurant: { city: @restaurant.city, latitude: @restaurant.latitude, longitude: @restaurant.longitude, name: @restaurant.name, price: @restaurant.price, type: @restaurant.type } }, as: :json
-    end
-
-    assert_response :created
-  end
-
-  test "should show restaurant" do
-    get restaurant_url(@restaurant), as: :json
-    assert_response :success
-  end
-
-  test "should update restaurant" do
-    patch restaurant_url(@restaurant), params: { restaurant: { city: @restaurant.city, latitude: @restaurant.latitude, longitude: @restaurant.longitude, name: @restaurant.name, price: @restaurant.price, type: @restaurant.type } }, as: :json
-    assert_response :success
-  end
-
-  test "should destroy restaurant" do
-    assert_difference("Restaurant.count", -1) do
-      delete restaurant_url(@restaurant), as: :json
-    end
-
-    assert_response :no_content
-  end
 end
